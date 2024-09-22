39d9d6cae472f492754ae02aa494175e0d6af7dd
skaqz <2481374335@qq.com>
2024-09-22 12:51:02 +0800

    fix:can't start bat procedure

--- Minecraft-Server-Start-Launcher/ServerPosition.cpp
+++ Minecraft-Server-Start-Launcher/ServerPosition.cpp
@@ -10,42 +10,42 @@
 
 void otwt_modforge_position()
 {
-	system("C:/Users/Administrator/Desktop/Minecraft Server/1.12.2-MOD-Forge/start.bat");
+	system("C:/Users/Administrator/Desktop/Minecraft_Server/1.12.2-MOD-Forge/start.bat");
 }
 
 void onit_modfabric_position()
 {
-	system("C:/Users/Administrator/Desktop/Minecraft Server/1.19.2-MOD-Fabric/start.bat");
+	system("C:/Users/Administrator/Desktop/Minecraft_Server/1.19.2-MOD-Fabric/start.bat");
 }
 
 void otwo_gregtech_position()
 {
-	system("C:/Users/Administrator/Desktop/Minecraft Server/1.20.1-GregTech/start.bat");
+	system("C:/Users/Administrator/Desktop/Minecraft_Server/1.20.1-GregTech/start.bat");
 }
 
 void otwo_modfabric_position()
 {
-	system("C:/Users/Administrator/Desktop/Minecraft Server/1.20.1-MOD-Fabric/start.bat");
+	system("C:/Users/Administrator/Desktop/Minecraft_Server/1.20.1-MOD-Fabric/start.bat");
 }
 
 void otwo_origin_position()
 {
-	system("C:/Users/Administrator/Desktop/Minecraft Server/1.20.1-Origin/start.bat");
+	system("C:/Users/Administrator/Desktop/Minecraft_Server/1.20.1-Origin/start.bat");
 }
 
 void otwo_survival_position()
 {
-	system("C:/Users/Administrator/Desktop/Minecraft Server/1.20.1-Survival/start.bat");
+	system("C:/Users/Administrator/Desktop/Minecraft_Server/1.20.1-Survival/start.bat");
 }
 
 void otwf_position()
 {
-	system("C:/Users/Administrator/Desktop/Minecraft Server/1.20.4/start.bat");
+	system("C:/Users/Administrator/Desktop/Minecraft_Server/1.20.4/start.bat");
 }
 
 void otwoo_origin_position()
 {
-	system("C:/Users/Administrator/Desktop/Minecraft Server/1.21.1-Origin/start.bat");
+	system("C:/Users/Administrator/Desktop/Minecraft_Server/1.21.1-Origin/start.bat");
 }
 
 
